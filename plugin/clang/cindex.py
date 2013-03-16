34c6657167307bf4835939324116945561a36bd1
Xavier Deguillard <deguilx@gmail.com>
2013-03-15 23:09:15 -0700

    Do not assert on 2 things at the same time.
    It makes debugging and understanding what could happen in #299 harder.

--- plugin/clang/cindex.py
+++ plugin/clang/cindex.py
@@ -1634,7 +1634,8 @@ class ClangObject(object):
     the ctypes library and the Clang CIndex library.
     """
     def __init__(self, obj):
-        assert isinstance(obj, c_object_p) and obj
+	assert obj
+        assert isinstance(obj, c_object_p)
         self.obj = self._as_parameter_ = obj
 
     def from_param(self):
@@ -1838,7 +1839,8 @@ class CCRStructure(Structure):
 
 class CodeCompletionResults(ClangObject):
     def __init__(self, ptr):
-        assert isinstance(ptr, POINTER(CCRStructure)) and ptr
+	assert ptr
+        assert isinstance(ptr, POINTER(CCRStructure))
         self.ptr = self._as_parameter_ = ptr
 
     def from_param(self):
